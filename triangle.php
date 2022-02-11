e9841448433c223915e09cf637ddc394278f6e64
Calomira <99393560+Calomira@users.noreply.github.com>
2022-02-11 10:10:53 +0100

    Add files via upload

---
+++ triangle.php
@@ -0,0 +1,7 @@
+<?php
+function TriangleArea($b,$h) {
+   return 0.5 * $b * $h;
+}
+ 
+echo "Triangle de hauteur de 10 cm par 10 cm de largeur contient un aire de ".TriangleArea(10, 10)." cm2<BR>"; 
+ ?>
